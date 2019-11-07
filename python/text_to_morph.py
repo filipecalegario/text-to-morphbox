64e1081f6d5ae4376eb1a1d00b96fa3e89a00165
Filipe Calegario <filipe.calegario@gmail.com>
2019-11-07 19:36:18 -0300

    Other MorphBox example

--- python/text_to_morph.py
+++ python/text_to_morph.py
@@ -6,6 +6,7 @@ from reportlab.lib.units import mm
 categories = {}
 font_size = 10
 main_font = 'Helvetica'
+main_font_bold = main_font + '-Bold'
 
 def text_to_dict():
     f = open('input.txt')
@@ -20,7 +21,7 @@ def text_to_dict():
 def calculate_max_title_width():
     max_title_width = 0;
     for names in categories:
-        current_width = stringWidth(names, main_font, font_size)
+        current_width = stringWidth(names, main_font_bold, font_size)
         if current_width > max_title_width:
             max_title_width = current_width
     return max_title_width
@@ -42,7 +43,9 @@ index_y = 0
 for category in categories:
     cell_width = (width - title_cell_width) / len(categories[category])
     c.rect(margin_horizontal, margin_vertical + index_y*cell_height, title_cell_width, cell_height)
-    c.drawString(margin_horizontal + 5, margin_vertical + (index_y*cell_height)+cell_height/2, category)
+    c.setFont(main_font_bold, font_size)
+    c.drawString(margin_horizontal + 5, margin_vertical + (index_y*cell_height)+cell_height/2+5, category)
+    c.setFont(main_font, font_size)
     list_of_values = categories[category]
     for i in range(len(list_of_values)):
         c.rect(margin_horizontal + title_cell_width+i*cell_width, margin_vertical + index_y*cell_height, cell_width, cell_height)
